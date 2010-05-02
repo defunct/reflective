1261f2a4a2694f449bcba29022e81ff9409bdef5
Alan Gutierrez <alan@blogometer.com>
2010-05-02 15:37:54 -0500

    Building with the new Mix.

--- src/mix/java/com/goodworkalan/reflective/mix/ReflectiveProject.java
+++ src/mix/java/com/goodworkalan/reflective/mix/ReflectiveProject.java
@@ -1,6 +1,5 @@
 package com.goodworkalan.reflective.mix;
 
-import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
@@ -10,10 +9,10 @@ public class ReflectiveProject extends ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.github.bigeasy.reflective/reflective/0.1"))
+                .produces("com.github.bigeasy.reflective/reflective/0.1")
                 .test()
                     .depends()
-                        .artifact(new Artifact("org.testng/testng-jdk15/5.10"))
+                        .include("org.testng/testng-jdk15/5.10")
                         .end()
                     .end()
                 .end()
