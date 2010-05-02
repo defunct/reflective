348657e0263ef81c1f5a52e878ed4855852486a5
Alan Gutierrez <alan@blogometer.com>
2010-05-02 14:20:42 -0500

    Updated Mix build.
     * New project artifact string.
     * Changed TestNG artifact string.

--- src/mix/java/com/goodworkalan/reflective/mix/ReflectiveProject.java
+++ src/mix/java/com/goodworkalan/reflective/mix/ReflectiveProject.java
@@ -10,10 +10,10 @@ public class ReflectiveProject extends ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.goodworkalan/reflective/0.1"))
+                .produces(new Artifact("com.github.bigeasy.reflective/reflective/0.1"))
                 .test()
                     .depends()
-                        .artifact(new Artifact("org.testng/testng/5.10/jdk15"))
+                        .artifact(new Artifact("org.testng/testng-jdk15/5.10"))
                         .end()
                     .end()
                 .end()
