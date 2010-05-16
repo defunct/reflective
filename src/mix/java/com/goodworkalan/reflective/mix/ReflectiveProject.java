b994ee79de5eb7b677354ef219e0fbe2248ac6a5
Alan Gutierrez <alan@blogometer.com>
2010-05-16 18:24:30 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/reflective/mix/ReflectiveProject.java
+++ src/mix/java/com/goodworkalan/reflective/mix/ReflectiveProject.java
@@ -4,8 +4,18 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class ReflectiveProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Reflective.
+ *
+ * @author Alan Gutierrez
+ */
+public class ReflectiveProject implements ProjectModule {
+    /**
+     * Build the project definition for Reflective.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
