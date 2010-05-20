ddbb3f61933c864d723a007282c24697cb053d1b
Alan Gutierrez <alan@blogometer.com>
2010-05-20 12:28:09 -0500

    Uniform versions across Reflective.

--- src/mix/java/com/goodworkalan/reflective/mix/ReflectiveExceptionProject.java
+++ src/mix/java/com/goodworkalan/reflective/mix/ReflectiveExceptionProject.java
@@ -19,7 +19,7 @@ public class ReflectiveExceptionProject implements ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces("com.github.bigeasy.reflective/reflective-exception/0.1")
+                .produces("com.github.bigeasy.reflective/reflective-exception/0.5")
                 .depends()
                     .production("com.github.bigeasy.reflective/reflective/0.+5")
                     .development("org.testng/testng-jdk15/5.10")
