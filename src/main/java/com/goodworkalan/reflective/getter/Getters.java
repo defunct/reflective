691ef52f3fb3ab13d073d5c1706e09b9681ab642
Alan Gutierrez <alan@blogometer.com>
2010-06-23 13:24:07 -0500

    Javadoc for Getters.

--- src/main/java/com/goodworkalan/reflective/getter/Getters.java
+++ src/main/java/com/goodworkalan/reflective/getter/Getters.java
@@ -10,23 +10,47 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+/**
+ * Utility class to obtain the getter members of classes.
+ * 
+ * @author Alan Gutierrez
+ */
 public class Getters {
-    /** The cache of classes to their list of getters. */
+    /** The cache of classes to their maps of getters. */
     private final static ConcurrentMap<Class<?>, Map<String, Getter>> GETTERS = new ConcurrentHashMap<Class<?>, Map<String, Getter>>();
     
-    static BeanInfo introspect(Class<?> beanClass, Class<?> stopClass) {
+    /**
+     * Get the bean properties for the given bean class. This method is
+     * extracted in order to test introspection exception handling.
+     * 
+     * @param beanClass
+     *            The bean class.
+     * @param flags
+     *            The flags to pass to the introspector.
+     * @return The bean information.
+     */
+    static BeanInfo introspect(Class<?> beanClass, int flags) {
         try {
-            return Introspector.getBeanInfo(beanClass, stopClass);
+            return Introspector.getBeanInfo(beanClass, flags);
         } catch (IntrospectionException e) {
             throw new RuntimeException(e);
         }
     }
 
+    /**
+     * Get a map of the getters indexed on property name for the public
+     * properties and fields exposed by the given bean class.
+     * 
+     * @param beanClass
+     *            The bean class.
+     * @return A list of getters for the public properties and fields of the
+     *         class.
+     */
     public static Map<String, Getter> getGetters(Class<?> type) {
         Map<String, Getter> getters = GETTERS.get(type);
         if (getters == null) {
             getters = new LinkedHashMap<String, Getter>();
-            BeanInfo beanInfo = introspect(type, null);
+            BeanInfo beanInfo = introspect(type, Introspector.USE_ALL_BEANINFO);
             for (PropertyDescriptor descriptor : beanInfo.getPropertyDescriptors()) {
                 java.lang.reflect.Method read = descriptor.getReadMethod();
                 if (read != null) {
