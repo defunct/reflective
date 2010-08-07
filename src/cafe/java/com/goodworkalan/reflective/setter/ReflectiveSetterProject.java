5da370e2cb63044516212075e5e3c85c2674c864
Alan Gutierrez <alan@blogometer.com>
2010-08-07 09:14:23 -0500

    Renamed Mix to Cafe.

--- src/cafe/java/com/goodworkalan/reflective/setter/ReflectiveSetterProject.java
+++ src/cafe/java/com/goodworkalan/reflective/setter/ReflectiveSetterProject.java
@@ -19,7 +19,7 @@ public class ReflectiveSetterProject implements ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces("com.github.bigeasy.reflective/reflective-setter/0.5.0.3")
+                .produces("com.github.bigeasy.reflective/reflective-setter/0.5.0.4")
                 .depends()
                     .development("org.testng/testng-jdk15/5.10")
                     .end()
